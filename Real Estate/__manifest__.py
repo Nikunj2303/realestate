9e6ecaf1f0a02fcc6fc633be898f6b494d466191
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2023-09-18 15:14:06 +0530

    odoo

--- Real Estate/__manifest__.py
+++ Real Estate/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-{
+var = {
     'name': 'Real Estate',
     'author': 'Reliution',
     'version': '15.0.0.1',
     'license': 'LGPL-3',
     'sequence': 1,
     'depends': ['base'],
-    'data': ['customer_addons\security\security.xml'],
+    'data': ['D:\odoo\customer_addons\Real Estate\security\model.access.csv'],
     'auto_install': False,
     'application': True,
-    'installable':True,
+    'installable': True,
 }
